0a47c42af6541bc43d5340e41c2bf73dbf0393e1
jc-bao <jc-bao@outlook.com>
2022-05-05 18:15:38 +0800

    add early termin control

--- agent.py
+++ agent.py
@@ -271,7 +271,7 @@ class AgentBase:
       end_info_dict = self.EP.info_parser(end_info)
       # TODO merge buffer and add parser
       # dropout unmoved experience
-      if getattr(end_info_dict, 'early_termin', False):
+      if getattr(end_info_dict, 'early_termin', False) and self.cfg.dropout_early_termin:
         useless_steps += traj_lens[i]
         continue
       if start_point < end_point:
