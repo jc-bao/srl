4a9fe43d982a060f90c41ac1876449c375b3373a
jc-bao <jc-bao@outlook.com>
2022-05-08 16:46:23 +0800

    add franka debug env

--- envs/franka_cube.py
+++ envs/franka_cube.py
@@ -1332,7 +1332,7 @@ if __name__ == '__main__':
 	run policy
 	'''
 	# env = gym.make('FrankaPNP-v0', num_envs=1, num_robots=2, num_cameras=0, headless=True, bound_robot=True, sim_device_id=1, rl_device_id=1, num_goals=1, inhand_rate=0.0)
-	env = FrankaCube(cfg_file='franka_debug.yaml', num_envs=1, num_robots=1, num_goals=1, num_cameras=0, headless=True, bound_robot=True, sim_device_id=0, rl_device_id=0, inhand_rate=0.5, os_rate=0)
+	env = FrankaCube(cfg_file='franka_debug.yaml', num_envs=8192, num_robots=1, num_goals=1, num_cameras=0, headless=True, bound_robot=True, sim_device_id=0, rl_device_id=0, inhand_rate=0.5, os_rate=0)
 	start = time.time()
 	# action_list = [
 	# 	*([[1,0,0,1]]*4), 
@@ -1340,7 +1340,7 @@ if __name__ == '__main__':
 	# 	*([[-1,0,0,1]]*4),
 	# 	*([[0,-1,0,1]]*4),]
 	obs = env.reset()
-	for i in range(100):
+	for i in range(10):
 		for j in range(env.cfg.max_steps):
 			if args.random:
 				act = torch.randn((env.cfg.num_envs,4*env.cfg.num_robots), device=env.device)
