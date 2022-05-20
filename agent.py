69b77944a0d185d7f7af8c8a5c1c8c660cb301a2
jc-bao <jc-bao@outlook.com>
2022-05-20 11:33:16 +0800

    fix curri resume bugs

--- agent.py
+++ agent.py
@@ -432,8 +432,8 @@ class AgentBase:
         if self.cfg.load_curri:
           for k, v in data['curri'].items():
             if k in self.cfg.curri:
-              print(f'[load] set {k} to {v["now"]}')
-              self.cfg.curri.now = v['now']
+              print(f'[Load] set {k} to {v["now"]}')
+              self.cfg['curri']['now'] = v['now']
       for name, obj in name_obj_list:
         obj.load_state_dict(data[name])
 
